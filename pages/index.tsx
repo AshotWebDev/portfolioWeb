a13f4d21d484ca3c48cb6206772f164d6c836263
AshotWebDev <poghosyan.01@list.ru>
2024-06-24 23:44:03 +0400

    three

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import AbouteMe from '@/components/AbouteMe'
-import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import Nav from '@/components/Nav'
@@ -36,7 +35,6 @@ const HomePage = () => {
       <Skils/>
       <Project/>
       <Price/>
-      <Contact/>
       <Footer/>
     </div>
   )
