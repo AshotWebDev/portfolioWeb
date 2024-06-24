0a0305cf1e51d99a5d43e963a97d7b95ba6bfb95
AshotWebDev <poghosyan.01@list.ru>
2024-06-24 23:00:51 +0400

    first

--- components/Helper/ProjectCard.tsx
+++ components/Helper/ProjectCard.tsx
@@ -12,7 +12,7 @@ const ProjectCard = ({image, title, tech1, tech2, tech3, tech4}: Props) => {
   return (
     <div className='grid w-[80%] mx-auto grid-cols-1 pt-[80px]  lg:grid-cols-2 gap-[30px] items-center'>
         <div className='p-4 rounded-xl relative cursor-pointer hover:-rotate-6 transform transition-all duration-200 bg-gray-800 shadow-md'>
-            <img src={image} alt={title}  className='object-contain rounded-md w-full h-[300px]' />
+            <Image src={image} alt={title} width={500} height={300}  className='object-contain rounded-md w-full h-[300px]' />
         </div>
         <div>
             <h1 className='text-white text-[25px]'>{title}</h1>
--- components/Helper/SkillCard.tsx
+++ components/Helper/SkillCard.tsx
@@ -10,7 +10,7 @@ const SkillCard = ({image, title, percent}: Props) => {
   return (
     <div className=' flex flex-col justify-between p-6  hover:bg-red-700 duration-300 transition-all cursor-pointer text-center rounded-lg bg-gray-900 h-[230px]'>
       <div className='flex flex-col justify-between h-[120px]'>
-         <img src={image} alt={title} width={80} className='object-cover mx-auto' />
+         <Image src={image} alt={title} width={80} height={80} className='object-cover mx-auto' />
         <h1 className='text-white text-[18px] mt-[10px] font-600'>{title}</h1>
       </div>
       <div className='bg-black mt-[10px] p-2 rounded-lg text-white opacity-40'>{percent}</div>
